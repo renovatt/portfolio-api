018438afb20642d83c995e121fa2b2968b108a3d
Will <wlymes@gmail.com>
2023-06-20 11:41:34 -0300

    feat: projetcs router added

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,10 +3,14 @@ import { ProjectController } from '../controllers/projects';
 
 const router = Router();
 
-const createProjects = new ProjectController().create;
 const projects = new ProjectController().projects;
+const createProjects = new ProjectController().create;
+const updateProjects = new ProjectController().update;
+const deleteProjects = new ProjectController().delete;
 
 router.get('/projects', projects);
 router.post('/projects', createProjects);
+router.put('/projects/:id', updateProjects);
+router.delete('/projects/:id', deleteProjects);
 
 export { router };
