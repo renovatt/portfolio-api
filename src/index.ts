a63136b436df6ca1db3f70923cfdcbad0a933790
Will <wlymes@gmail.com>
2023-06-24 22:53:07 -0300

    feat: implemented database connection

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import 'express-async-errors';
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
 import { router } from './routers';
 import { errorHandlerMiddleware } from './middlewares/errosHandler';
 
+dotenv.config();
+
 const PORT = process.env.PORT;
+const DATABASE_URL = process.env.DATABASE_URL;
+
 const server = express();
 
 server.use(express.json());
 server.use(router);
 server.use(errorHandlerMiddleware);
 
-server.listen(PORT || 5000, () => console.log(`server listening on port ${PORT}`));
+mongoose
+    .connect(DATABASE_URL as string)
+    .then(() => {
+        console.log('Connection to MongoDB successfully established!');
+        server.listen(
+            {
+                port: PORT,
+                host: '0.0.0.0',
+            },
+            () => {
+                console.log(`HTTP server running on http://localhost: ${PORT} 🚀`);
+            },
+        );
+    });
