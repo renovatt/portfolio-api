e04dfa0c905878de38266e3f7aa37f4607ef2d47
Will <wlymes@gmail.com>
2023-07-16 10:01:40 -0300

    feat: added retry blocking

--- src/middlewares/auth/index.ts
+++ src/middlewares/auth/index.ts
@@ -3,6 +3,11 @@ import { NextFunction, Request, Response } from 'express';
 import { compareSecretKey, decryptSecretKey } from '../../connections';
 import { BadRequestError, InternalError, UnauthorizedError } from '../../errors';
 
+const MAX_FAILED_ATTEMPTS = 3;
+const LOCKOUT_DURATION = 10 * 60 * 1000; // 10 minutes in milliseconds
+const failedAttempts: Map<string, number> = new Map();
+const lockedOutUsers: Set<string> = new Set();
+
 export const auth = () => async (
     req: Request,
     res: Response,
@@ -13,7 +18,7 @@ export const auth = () => async (
         req.method === 'PUT' ||
         req.method === 'DELETE'
     ) {
-
+        const ipAddress = req.ip;
         const secretKey = req.headers['secret-key'];
 
         try {
@@ -24,18 +29,42 @@ export const auth = () => async (
                     .json({ error: badRequestError.message });
             }
 
+            if (lockedOutUsers.has(ipAddress)) {
+                const lockoutError = new UnauthorizedError('Você está bloqueado devido a tentativas incorretas.');
+                return res
+                    .status(lockoutError.statusCode)
+                    .json({ error: lockoutError.message });
+            }
+
             const mySecretKeyHash = await getSecretKey();
             const decryptedPrivateKey = decryptSecretKey(String(secretKey));
             const isMatch = await compareSecretKey(decryptedPrivateKey, mySecretKeyHash);
 
             if (!isMatch) {
-                const methodUnauthorizedError = new UnauthorizedError('Permissão negada!');
+                const attempts = failedAttempts.get(ipAddress) || 0;
+                if (attempts >= MAX_FAILED_ATTEMPTS) {
+                    lockedOutUsers.add(ipAddress);
+                    setTimeout(() => {
+                        lockedOutUsers.delete(ipAddress);
+                        failedAttempts.delete(ipAddress);
+                    }, LOCKOUT_DURATION);
+
+                    const lockoutError = new UnauthorizedError('Acesso bloqueado devido a tentativas incorretas.');
+                    return res
+                        .status(lockoutError.statusCode)
+                        .json({ error: lockoutError.message });
+                }
+
+                failedAttempts.set(ipAddress, attempts + 1);
+
+                const unauthorizedError = new UnauthorizedError('PERMISSÃO NEGADA!');
                 return res
-                    .status(methodUnauthorizedError.statusCode)
-                    .json({ error: methodUnauthorizedError.message });
+                    .status(unauthorizedError.statusCode)
+                    .json({ error: unauthorizedError.message });
             }
 
-            next();
+            failedAttempts.delete(ipAddress);
+
         } catch (error) {
             console.error(error);
             const internalError = new InternalError('Ocorreu um erro no servidor.');
@@ -43,7 +72,6 @@ export const auth = () => async (
                 .status(internalError.statusCode)
                 .json({ error: internalError.message });
         }
-    } else {
-        next();
     }
+    next();
 };
