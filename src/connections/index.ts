9d28157da2f4bc00fb1c8491479a02f07c44a612
Will <wlymes@gmail.com>
2023-07-16 09:45:14 -0300

    feat: bcrypt and crypto functions created

--- src/connections/index.ts
+++ src/connections/index.ts
@@ -1,4 +1,12 @@
+import bcrypt from 'bcrypt';
+import crypto from 'crypto';
+import dotenv from 'dotenv';
 import { BadRequestError } from '../errors';
+
+dotenv.config();
+
+const SECRET = process.env.SECRET as string;
+const ENCRYPTION_ALGORITHM = process.env.ENCRYPTION_ALGORITHM as string;
 
 const verifyMongoId = (userId: string) => {
     const idRegex = /^[0-9a-fA-F]{24}$/;
@@ -10,3 +18,24 @@ export function validateId(id: string): void {
         throw new BadRequestError('ID inválido ou não existe!');
     }
 }
+
+export const compareSecretKey = async (
+    secretKey: string,
+    mySecretKeyHash: string
+): Promise<boolean> => {
+    return bcrypt.compare(secretKey, mySecretKeyHash);
+};
+
+export const decryptSecretKey = (encrypted: string) => {
+    const [iv, encryptedSecretKey] = encrypted.split(':');
+
+    const decipher = crypto.createDecipheriv(
+        ENCRYPTION_ALGORITHM,
+        Buffer.from(SECRET),
+        Buffer.from(iv, 'hex')
+    );
+
+    let decrypted = decipher.update(encryptedSecretKey, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted.toString();
+};
